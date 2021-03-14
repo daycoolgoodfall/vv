a7575fb0f32e2d39a514031ad99a546f0ef7bcf1
daycoolgoodfall <55581427+daycoolgoodfall@users.noreply.github.com>
2021-03-14 19:22:34 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,10 +5,10 @@ mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v3.1/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 mv /tmp/v2ray/v2ray-v3.1-linux-64  /tmp/v2ray/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-install -m 755 /tmp/v2ray/geoip.dat /usr/local/bin/geoip.dat
-install -m 755 /tmp/v2ray/geosite.dat /usr/local/bin/geosite.dat
+install -m 755 /tmp/v2ray/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/v2ray/geoip.dat /usr/local/bin/geoip.dat
+install -m 755 /tmp/v2ray/v2ray/geosite.dat /usr/local/bin/geosite.dat
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
